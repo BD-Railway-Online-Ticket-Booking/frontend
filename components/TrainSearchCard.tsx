29452b4b4e26f4cbba4f61ffa21d76750f344ecf
Mahmud-Araf <almahmudaraf@gmail.com>
2024-08-12 16:23:22 +0600

    target is removed

--- components/TrainSearchCard.tsx
+++ components/TrainSearchCard.tsx
@@ -10,69 +10,81 @@ export default function TrainSearchCard({ train, date }: any) {
     const [isSeat, setIsSeat] = useState(false);
     const [isRoute, setIsRoute] = useState(false);
     const [seats, setSeats] = useState<any[]>([]);
+    const [isExpanded, setIsExpanded] = useState(false);
 
-    async function fetchAvailableSeats() {
-        const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
-        const response = await fetch(`${ENDPOINT}/booking/train/${train.train_id}/available/seats?date=${date}&dflag=${train.dflag}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const data = await response.json();
-
-        setSeats(data);
+    const onToggleExpand = () => {
+        if (isExpanded) setIsExpanded(false);
+        else setIsExpanded(true);
     }
 
-    useEffect(() => {
-        fetchAvailableSeats();
-    }, [isSeat]);
+        async function fetchAvailableSeats() {
+            const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
+            const response = await fetch(`${ENDPOINT}/booking/train/${train.train_id}/available/seats?date=${date}&dflag=${train.dflag}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
 
-    return (
-        <>
-            {
-                seats.length > 0 && (
-                    <div className="p-4 border-2 bg-[var(--p-bg)] shadow-lg rounded-lg mx-4 my-1 flex flex-col md:flex-row justify-center md:justify-between items-center">
-                        <h1 className="text-[var(--sec-bg)] text-lg md:text-2xl font-semibold p-3 md:m-5 text-nowrap">{train.train_name}</h1>
-                        <div className="flex justify-around items-center">
-                            <Button className="bg-blue-500 text-xs md:text-lg p-2 md:p-4 mx-3 my-2" onClick={
-                                () => {
-                                    if (isSeat) setIsSeat(false);
-                                    else {
-                                        setIsSeat(true);
-                                    }
-                                }
-                            }>Book Seats</Button>
-                            <Button className="bg-orange-700 text-xs md:text-lg p-2 md:p-4 mx-3 my-2" onClick={()=>{
-                                if(isRoute) setIsRoute(false);
-                                else setIsRoute(true);
-                            }}>Show Routes</Button>
+            const data = await response.json();
+
+            setSeats(data);
+        }
+
+        useEffect(() => {
+            fetchAvailableSeats();
+        }, [isSeat]);
+
+        return (
+            <>
+                {
+                    seats.length > 0 && (
+                        <div className="bg-[var(--p-bg)] border-2 shadow-lg flex justify-between items-center rounded-lg m-5">
+                            <h2 className="text-[var(--sec-bg)] text-lg md:text-2xl font-semibold tracking-wider p-3 m-5 text-nowrap">{train.train_name}</h2>
+                            <button className="text-xl text-[var(--sec-bg)] p-3 m-5" onClick={onToggleExpand}>
+                                {isExpanded ? '▲' : '▼'}
+                            </button>
                         </div>
-                    </div>
-                )
-            }
+                    )
+                }
 
-            {
-                isSeat && seats.length > 0 && (
-                    <div className="flex justify-evenly items-center overflow-x-auto whitespace-nowrap mb-8">
-                        {
-                            seats.map((seat: any) => (
-                                seat.available > 0 && (
-                                    <SearchSeatCard key={seat.id} seat={seat} date={date} path={train.path} dflag={train.dflag} />
-                                )
-                            ))
+                {isExpanded && (<div className="flex justify-center items-center">
+                    <Button className="bg-blue-500 text-lg p-4 m-5" onClick={
+                        () => {
+                            if (isSeat) setIsSeat(false);
+                            else {
+                                setIsSeat(true);
+                            }
                         }
-                    </div>
+                    }>Book Seats</Button>
+                    <Button className="bg-orange-700 text-lg p-4 m-5" onClick={() => {
+                        if (isRoute) setIsRoute(false);
+                        else setIsRoute(true);
+                    }}>Show Routes</Button>
+                </div>
                 )
-            }
-            {
-                isRoute && (
-                    <div>
-                        <SearchTrainRouteGraph path={train.path} />
-                    </div>
-                )
-            }
-        </>
-    )
-}
+                }
+
+                {
+                   isExpanded && isSeat && seats.length > 0 && (
+                        <div className="flex justify-evenly items-center overflow-x-auto whitespace-nowrap mb-8">
+                            {
+                                seats.map((seat: any) => (
+                                    seat.available > 0 && (
+                                        <SearchSeatCard key={seat.id} seat={seat} date={date} path={train.path} dflag={train.dflag} />
+                                    )
+                                ))
+                            }
+                        </div>
+                    )
+                }
+                {
+                   isExpanded && isRoute && (
+                        <div>
+                            <SearchTrainRouteGraph path={train.path} />
+                        </div>
+                    )
+                }
+            </>
+        )
+    }
